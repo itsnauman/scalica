5832c4d1c3fe3a579fd2654f3f9a3bbf42f961d4
Abrar Ahmed <abrarahmed257@gmail.com>
2018-12-09 23:49:18 +0000

    Sentiment works with mapper

--- scalica/web/scalica/utils/sentiment.py
+++ scalica/web/scalica/utils/sentiment.py
@@ -3,19 +3,18 @@ from pyspark import SparkContext
 
 sc = SparkContext()
 textfile = sc.textFile("hashtags.txt")
-client = language.LanguageServiceClient()
 
 def map_hashtags(line):
-    hashtag = line.value[1:line.value.index(":")]
+    hashtag = line[1:line.index(":")]
     document = language.types.Document(
-        content=line[line.index(":")],
+        content=line[line.index(":")+1],
         type=language.enums.Document.Type.PLAIN_TEXT,
     )
+    client = language.LanguageServiceClient()
     full_sentiment = client.analyze_sentiment(document=document).document_sentiment
     score = full_sentiment.score
     magnitude = full_sentiment.magnitude
-    sentiment = (score, magnitude, 1)
-    return (hashtag, sentiment)
+    return (hashtag, (score, magnitude, 1))
 
 def reduce_hashtags(sentiment1, sentiment2):
     return (sentiment1[0] + sentiment2[0], sentiment1[1] + sentiment2[1], sentiment1[2] + sentiment2[2])
@@ -25,9 +24,8 @@ def take_avg(hash_with_sentiment):
     sentiment = hash_with_sentiment[1]
     return hashtag, (sentiment[0] / sentiment[2], sentiment[1] / sentiment[2])
 
-result = textfile.map(map_hashtags).reduceByKey(reduce_hashtags).map(take_avg).collect()
+result = textfile.map(map_hashtags)\
+                 .reduceByKey(reduce_hashtags)\
+                 .map(take_avg)
 
-print '\n\n\n\n\n\n\n\n'
-print result
 result.saveAsTextFile("results.txt")
-print '\n\n\n\n\n\n\n\n'
