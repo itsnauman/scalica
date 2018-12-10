7620d36db703da71d6fa7f3349409d1e4f9933a5
Nauman Ahmad <nauman-ahmad@outlook.com>
2018-12-10 17:11:25 -0500

    Update

--- grpc-server/hashtag/cron_job.py
+++ grpc-server/hashtag/cron_job.py
@@ -1,26 +1,18 @@
-import schedule
+#!/usr/bin/python
+
 import time
 from sentiment import map_hashtags, reduce_hashtags, take_avg
 from pyspark import SparkContext
+
 import redis
 
 r = redis.Redis(host='35.243.217.152', port=6379, db=0)
 sc = SparkContext()
 
+textfile = sc.textFile("tweets_dump.txt")
+result = textfile.flatMap(map_hashtags)\
+                 .reduceByKey(reduce_hashtags)\
+                 .map(take_avg)
 
-def job():
-    textfile = sc.textFile("tweets_dump.txt")
-    result = textfile.flatMap(map_hashtags)\
-                     .reduceByKey(reduce_hashtags)\
-                     .map(take_avg)
-
-    for hashtag, (sentiment_score, sentiment_magnitude) in result.collect():
-        r.set("@score@" + hashtag, sentiment_score)
-
-job()
-
-schedule.every(30).minutes.do(job)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+for hashtag, (sentiment_score, sentiment_magnitude) in result.collect():
+    r.set("@score@" + hashtag, sentiment_score)
