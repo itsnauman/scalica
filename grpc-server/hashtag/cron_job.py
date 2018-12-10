733552843ca37e73c2c1b270bd918f10675c07df
Abrar Ahmed <abrarahmed257@gmail.com>
2018-12-10 15:47:30 +0000

    Push sentiment to redis

--- grpc-server/hashtag/cron_job.py
+++ grpc-server/hashtag/cron_job.py
@@ -2,15 +2,21 @@ import schedule
 import time
 from sentiment import map_hashtags, reduce_hashtags, take_avg
 from pyspark import SparkContext
+import redis
+
+r = redis.Redis(host='localhost', port=6379, db=0)
+sc = SparkContext()
+
 
 def job():
-    sc = SparkContext()
     textfile = sc.textFile("hashtags.txt")
     result = textfile.flatMap(map_hashtags)\
                      .reduceByKey(reduce_hashtags)\
                      .map(take_avg)
 
-    result.saveAsTextFile("results.txt")
+    for hashtag, (sentiment_score, sentiment_magnitude) in result.collect():
+        r.rpush("hashtag_sentiment_score:" + hashtag, sentiment_score)
+        r.rpush("hashtag_sentiment_magnitude:" + hashtag, sentiment_magnitude)
 
 schedule.every(30).minutes.do(job)
 
